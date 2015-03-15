528450665663983ff4fc2c9e4e930f82410b9f50
Pugiemonn <pugiemonn@gmail.com>
2015-03-15 22:51:11 +0900

    events_controllerのindexとshowをを編集しました

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,12 +4,13 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.order(id: :desc).limit(10)
   end
 
   # GET /events/1
   # GET /events/1.json
   def show
+    @event = Event.find(params[:id])
   end
 
   # GET /events/new
