c22fbe9f93eb58fe86b8e4178a96f55e9c0bca8d
Pugiemonn <pugiemonn@gmail.com>
2015-03-15 23:25:45 +0900

    updateアクションが通らないのでpermitを追加しました。

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -70,6 +70,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params[:event]
+      params.permit(:id)
     end
 end
