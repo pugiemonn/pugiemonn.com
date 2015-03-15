40a03740b63eb74b2e6b7b0fab87aa32524253e9
Pugiemonn <pugiemonn@gmail.com>
2015-03-16 00:17:31 +0900

    event_urlをformから投げるように変更しました。

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -70,6 +70,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.permit(:id)
+      params.require(:event).permit(:event_url)
     end
 end
