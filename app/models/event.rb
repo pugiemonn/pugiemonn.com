912a11393c62f8841be9f9fcd8e96daefc00c130
Pugiemonn <pugiemonn@gmail.com>
2015-03-20 02:42:11 +0900

    イベントモデルにリレーションを追加しました

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,10 @@
 class Event < ActiveRecord::Base
   validates :event_url, presence: true, uniqueness: true
+
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def self.tagged_with(name)
+    Tag.find_by_name!(name).events
+  end
 end
