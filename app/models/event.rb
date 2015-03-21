ea08bffb3f36be64a9a3f0977770050acfeed0ad
Pugiemonn <pugiemonn@gmail.com>
2015-03-21 23:55:39 +0900

    イベントモデルにメソッドを追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,4 +7,19 @@ class Event < ActiveRecord::Base
   def self.tagged_with(name)
     Tag.find_by_name!(name).events
   end
+
+  def self.tag_counts
+    Tag.select("tags.*, count(taggins.tag_id) as count").
+    joins(:taggings).group("taggings.tag_id")
+  end
+
+  def tag_list
+    tags.map(&:name).join(", ")
+  end
+
+  def tag_list=(names)
+    self.tags = names.split(",").map do |t|
+      Tag.where(name: t.strip).first_or_crearte!
+    end
+  end
 end
