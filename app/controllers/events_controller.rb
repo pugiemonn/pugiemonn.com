d8939c8edb850f29e50565c4ef19b2ed5b4925c7
Pugiemonn <pugiemonn@gmail.com>
2015-04-27 07:57:14 +0900

    event_paramsに:catchを追加しました

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -75,6 +75,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :place, :event_url, :started_at, :ended_at,:tag_list)
+      params.require(:event).permit(:title, :place, :event_url, :started_at, :ended_at, :catch, :tag_list)
     end
 end
