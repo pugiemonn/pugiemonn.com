2f9ec394abe8e33e40286794ac3354acc0d3f3c2
Pugiemonn <pugiemonn@gmail.com>
2015-05-10 22:16:02 +0900

    退会機能を追加しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
+  before_destroy :check_all_events_finished
 
-  has_many :created_events, class_name: 'Event', foreign_key: :owner_id
-  has_many :tickets
+  has_many :created_events, class_name: 'Event', foreign_key: :owner_id, dependent: :nullify
+  has_many :tickets, dependent: :nullify
+  has_many :participating_events, through: :tickets, source: :event
 
   def self.find_or_create_from_auth_hash(auth_hash)
     provider  = auth_hash[:provider]
@@ -13,5 +15,19 @@ class User < ActiveRecord::Base
       user.nickname  = nickname
       user.image_url = image_url
     end
+  end
+
+  private
+
+  def check_all_events_finished
+    now = Time.zone.now
+    if created_events.where(':now < ended_at', now: now).exists?
+      errors[:base] << '公開中の未終了のイベントが存在します。'
+    end
+
+    if participating_events.where(':now < ended_at', now: now).exists?
+      errors[:base] << '未終了の参加イベントが存在します。'
+    end
+    errors.blank?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
     resources :tickets
   end
 
+  resource :user do
+    get 'retire'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
