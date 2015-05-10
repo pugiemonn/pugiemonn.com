288c4bf5d06af9b328913048c22713440a8b1d0c
Pugiemonn <pugiemonn@gmail.com>
2015-05-10 13:04:23 +0900

    イベントへの参加者一覧を表示するようにしました

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,6 +15,7 @@ class EventsController < ApplicationController
   # GET /events/1.json
   def show
     @event = Event.find(params[:id])
+    @tickets = @event.tickets.includes(:user).order(:created_at)
   end
 
   # GET /events/new
