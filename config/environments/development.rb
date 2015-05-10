1a5239fec003037d6b895b95ae830c2293d73cca
Pugiemonn <pugiemonn@gmail.com>
2015-05-10 20:55:22 +0900

    Bulletの設定をdevelopment.rbに追加しました

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,17 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Bullet settings
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.bugsnag = true
+    Bullet.airbrake = true
+    Bullet.add_footer = true
+    Bullet.stacktrace_includes = [ 'your_gem', 'your_middleware' ]
+  end
 end
