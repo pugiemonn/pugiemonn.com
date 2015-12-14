dcd9f479f245e05e7c8158401670e839681ef438
Pugiemonn <pugiemonn@gmail.com>
2015-12-14 11:14:45 +0900

    リンクをhttpsにも対応

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   require "uri"
 
   def text_url_to_link text
-    URI.extract(text, ['http']).uniq.each do |url|
+    URI.extract(text, ['http', 'https']).uniq.each do |url|
       sub_text = ""
       sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
 
