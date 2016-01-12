9a38a4247bd16100ca3c6dfa84929a061e18ea60
Pugiemonn <pugiemonn@gmail.com>
2016-01-12 23:23:00 +0900

    リンクタグによるデザインくずれを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
   def text_url_to_link text
     URI.extract(text, ['http', 'https']).uniq.each do |url|
       sub_text = ""
-      sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+      sub_text << "<a href=" << url << " target=\"_blank\" class=\"wob-ba\">" << url << "</a>"
 
       text.gsub!(url, sub_text)
     end
