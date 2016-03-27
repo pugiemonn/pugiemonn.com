48766cca7b713d8ecf9deea07b55a2ecbf3c492d
Pugiemonn <pugiemonn@gmail.com>
2016-03-27 17:10:20 +0900

    ActiveAdminの初期ファイルをコミット

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.create!(email: ENV["PUGIEMONN_COM_ADMIN_ID"], password: ENV["PUGIEMONN_COM_ADMIN_PASS"], password_confirmation: ENV["PUGIEMONN_COM_ADMIN_PASS"])
