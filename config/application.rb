8f15e69b0b115b5e147c6af472b727b00047b39c
Pugiemonn <pugiemonn@gmail.com>
2016-07-17 10:48:04 +0900

    前コミットで消えてしまった設定を記述しました。

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,13 @@ Bundler.require(*Rails.groups)
 
 module PugiemonnCom
   class Application < Rails::Application
+    config.eager_load_paths += Dir["#{config.root}/lib"]
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    config.time_zone = 'Tokyo'
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
